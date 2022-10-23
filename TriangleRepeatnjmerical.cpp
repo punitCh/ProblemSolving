5471e3a6b00b63deb7b03e7dfa5e421b09875440
punitCh <116084539+punitCh@users.noreply.github.com>
2022-10-23 12:01:37 +0530

    Create TriangleRepeatnjmerical.cpp

---
+++ TriangleRepeatnjmerical.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    
+    while(i<=n)
+    {   int space=n-i;
+        int j=1;
+        while(space)
+        {
+           cout<<" ";
+           space--;
+        }
+        while(j<=i)
+        {
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+}
