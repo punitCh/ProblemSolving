0f5da4bdc8314a507fa02ed8e05400f41cc0e1c8
punitCh <116084539+punitCh@users.noreply.github.com>
2022-10-23 12:34:53 +0530

    Create NumIncreasingTriangle.cpp

---
+++ NumIncreasingTriangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    
+    while(i<=n)
+    {   int space=i-1;
+        int j=1;
+        int start=j+1;
+        while(space)
+        {
+           cout<<" ";
+           space--;
+        }
+        while(j<=n-i+1)
+        {
+            cout<<i+j-1;
+            
+            
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+}
