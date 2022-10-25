e17d2d924ac08efabda8037a0ced6e605125c972
punitCh <116084539+punitCh@users.noreply.github.com>
2022-10-25 21:30:36 +0530

    Create AssignArrayas1InAllIndex.cpp

---
+++ AssignArrayas1InAllIndex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int n[10];
+    
+    for(int i=0;i<10;i++)
+    {
+        n[i]=1;
+        cout<<" Value at "<<i<<" is " << n[i] << endl;
+    }
+    
+    
+}
